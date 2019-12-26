95bce28586bca25a3d0f959b98bedf5fd7d38c48
paramagicdev <konnor5456@gmail.com>
2019-12-26 06:32:10 -0500

    Added Guardfile and some basic views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(page_title: '')
+    base_title = 'PCR Creator'
+
+    return base_title if page_title.empty?
+
+    "#{page_title} | #{base_title}"
+  end
 end
