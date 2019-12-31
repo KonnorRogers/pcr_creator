a4a78f559b38a9e7d13e08e4f5856bcad04cf153
paramagicdev <konnor5456@gmail.com>
2019-12-31 01:15:27 -0500

    add procfile, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#index'
 end
