c19c69c7c138262652e69643f219fe1c07a8771b
paramagicdev <konnor5456@gmail.com>
2020-01-08 23:06:50 -0500

    add validations to address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :patient
   belongs_to :report
+
+  validates :room_number, length: { maximum: 50 }
+
+  columns = %i[address city country state zip_code]
+  columns.each do |column|
+    validates column, length: { maximum: 255 }, presence: true
+  end
 end
