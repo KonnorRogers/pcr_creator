7832ec53616a5619cf90fd8bb824de4e763d106e
paramagicdev <konnor5456@gmail.com>
2020-01-12 11:43:51 -0500

    update validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,9 +3,10 @@ class Place < ApplicationRecord
   # belongs_to :report
 
   validates :room_number, length: { maximum: 50 }
+  validates :zip_code, length: { is: 5 }, presence: true
 
-  columns = %i[address city country state zip_code]
+  columns = %i[address city country state]
   columns.each do |column|
-    validates column, length: { maximum: 255 }, presence: true
+    validates column, length: { maximum: 500 }, presence: true
   end
 end
